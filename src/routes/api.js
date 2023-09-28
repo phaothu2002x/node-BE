5ceb8c672305c34bc09414de0797b3c1f16a2808
phaothu2002x <truongthuan21342@gmail.com>
2023-09-28 16:26:32 +0700

    kiem tra quyen han User (middleware)

--- src/routes/api.js
+++ src/routes/api.js
@@ -3,6 +3,7 @@ import express from "express";
 import apiController from "../controllers/apiController";
 import userController from "../controllers/userController";
 import groupController from "../controllers/groupController";
+import { checkUserJWT, checkUserPermission } from "../middleware/JWTAction";
 const router = express.Router();
 
 /**
@@ -14,15 +15,29 @@ const tesMiddleWare = (req, res, next) => {
     console.log("calling middleware");
     next();
 };
+
+// const checkUserLogin = (req, res, next) => {
+//     const nonSecurePaths = ["/", "/register", "/login"];
+//     if (nonSecurePaths.includes(req.path)) return next();
+
+//     //authenticate user
+//     next();
+// };
+
 const initAPIRoutes = (app) => {
     //path, handler
     // rest API
     //get --R, post- C, put-U, delete-D
-    router.get("/test-api", apiController.testApi);
+
     router.post("/register", apiController.handleRegister);
     router.post("/login", apiController.handleLogin);
 
-    router.get("/user/read", userController.readFunc);
+    router.get(
+        "/user/read",
+        checkUserJWT,
+        checkUserPermission,
+        userController.readFunc
+    );
     router.post("/user/create", userController.createFunc);
     router.put("/user/update", userController.updateFunc);
     router.delete("/user/delete", userController.deleteFunc);
