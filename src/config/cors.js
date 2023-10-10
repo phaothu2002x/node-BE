f55fcb49e45850f0b44e839b7951712c5628a525
phaothu2002x <truongthuan21342@gmail.com>
2023-10-11 01:12:51 +0700

    bear authentication

--- src/config/cors.js
+++ src/config/cors.js
@@ -3,8 +3,9 @@ require("dotenv").config();
 const configCors = (app) => {
     // Add headers before the routes are defined
     app.use(function (req, res, next) {
+        // console.log(req.method);
         // Website you wish to allow to connect
-        res.setHeader("Access-Control-Allow-Origin", process.env.REACRT_URL);
+        res.setHeader("Access-Control-Allow-Origin", process.env.REACT_URL);
 
         // Request methods you wish to allow
         res.setHeader(
@@ -15,13 +16,16 @@ const configCors = (app) => {
         // Request headers you wish to allow
         res.setHeader(
             "Access-Control-Allow-Headers",
-            "X-Requested-With,content-type"
+            "X-Requested-With,content-type, Authorization"
         );
 
         // Set to true if you need the website to include cookies in the requests sent
         // to the API (e.g. in case you use sessions)
         res.setHeader("Access-Control-Allow-Credentials", true);
 
+        if (req.method === "OPTIONS") {
+            return res.sendStatus(200);
+        }
         // Pass to next layer of middleware
         next();
     });
